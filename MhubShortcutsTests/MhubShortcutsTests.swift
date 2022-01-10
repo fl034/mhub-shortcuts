42bd857e44026c8ff13d7758a00a70d7199a1cfd
Frank Lehmann <hey@heyfrank.de>
2022-01-10 14:23:39 +0100

    Init project

---
+++ MhubShortcutsTests/MhubShortcutsTests.swift
@@ -0,0 +1,33 @@
+//
+//  MhubShortcutsTests.swift
+//  MhubShortcutsTests
+//
+//  Created by Frank Lehmann on 10.01.22.
+//
+
+import XCTest
+@testable import MhubShortcuts
+
+class MhubShortcutsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
